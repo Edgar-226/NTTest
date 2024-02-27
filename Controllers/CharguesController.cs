072167c2a18d6d0a5e61f1a3d9d20f123877acc4
edgar <edgar.bast.95@gmail.com>
2024-02-27 02:22:53 -0600

    Update: Avances Lectura csv

--- Controllers/CharguesController.cs
+++ Controllers/CharguesController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using CsvHelper.Configuration;
+using CsvHelper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -22,119 +25,119 @@ namespace NTTest.Controllers
             _db = context;
         }
 
-        // GET: api/Chargues
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Company>>> GetCompany()
-        {
-            if (_db.Company == null)
-            {
-                return NotFound();
-            }
-            return await _db.Company.ToListAsync();
-        }
+        //// GET: api/Chargues
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<Company>>> GetCompany()
+        //{
+        //    if (_db.Company == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return await _db.Company.ToListAsync();
+        //}
 
-        // GET: api/Chargues/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Company>> GetCompany(string id)
-        {
-            if (_db.Company == null)
-            {
-                return NotFound();
-            }
-            var company = await _db.Company.FindAsync(id);
+        //// GET: api/Chargues/5
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<Company>> GetCompany(string id)
+        //{
+        //    if (_db.Company == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var company = await _db.Company.FindAsync(id);
 
-            if (company == null)
-            {
-                return NotFound();
-            }
+        //    if (company == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return company;
-        }
+        //    return company;
+        //}
 
-        // PUT: api/Chargues/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutCompany(string id, Company company)
-        {
-            if (id != company.company_id)
-            {
-                return BadRequest();
-            }
+        //// PUT: api/Chargues/5
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutCompany(string id, Company company)
+        //{
+        //    if (id != company.company_id)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            _db.Entry(company).State = EntityState.Modified;
+        //    _db.Entry(company).State = EntityState.Modified;
 
-            try
-            {
-                await _db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CompanyExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await _db.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!CompanyExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        // POST: api/Chargues
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Company>> PostCompany(Company company)
-        {
-            if (_db.Company == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Company'  is null.");
-            }
-            _db.Company.Add(company);
-            try
-            {
-                await _db.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (CompanyExists(company.company_id))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //// POST: api/Chargues
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPost]
+        //public async Task<ActionResult<Company>> PostCompany(Company company)
+        //{
+        //    if (_db.Company == null)
+        //    {
+        //        return Problem("Entity set 'ApplicationDbContext.Company'  is null.");
+        //    }
+        //    _db.Company.Add(company);
+        //    try
+        //    {
+        //        await _db.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateException)
+        //    {
+        //        if (CompanyExists(company.company_id))
+        //        {
+        //            return Conflict();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return CreatedAtAction("GetCompany", new { id = company.company_id }, company);
-        }
+        //    return CreatedAtAction("GetCompany", new { id = company.company_id }, company);
+        //}
 
-        // DELETE: api/Chargues/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCompany(string id)
-        {
-            if (_db.Company == null)
-            {
-                return NotFound();
-            }
-            var company = await _db.Company.FindAsync(id);
-            if (company == null)
-            {
-                return NotFound();
-            }
+        //// DELETE: api/Chargues/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteCompany(string id)
+        //{
+        //    if (_db.Company == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var company = await _db.Company.FindAsync(id);
+        //    if (company == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            _db.Company.Remove(company);
-            await _db.SaveChangesAsync();
+        //    _db.Company.Remove(company);
+        //    await _db.SaveChangesAsync();
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        private bool CompanyExists(string id)
-        {
-            return (_db.Company?.Any(e => e.company_id == id)).GetValueOrDefault();
-        }
+        //private bool CompanyExists(string id)
+        //{
+        //    return (_db.Company?.Any(e => e.company_id == id)).GetValueOrDefault();
+        //}
 
         #endregion
 
@@ -158,10 +161,19 @@ namespace NTTest.Controllers
                 return response;
             }
 
-            var pathCsv = "D:/Repos/NTTest/CSV/data_prueba_tecnica.csv";
+            var pathCsv = "D:\\Repos\\NTTest\\CSV\\data_prueba_tecnica.csv";
 
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                PrepareHeaderForMatch = args => args.Header.ToLower(),
+            };
+            using (var reader = new StreamReader(pathCsv))
+            using (var csv = new CsvReader(reader, config))
+            {
+                var records = csv.GetRecords<Csv>();
 
-
+                var companiesIds = records.Select(s => s.company_id).Distinct().ToList();
+            }
             return response;
         }
 
---
+++ Models/Csv.cs
@@ -0,0 +1,17 @@
+﻿namespace NTTest.Models
+{
+    public class Csv
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string company_id { get; set; }
+        public string amount { get; set; }
+
+        public string status { get; set; }
+
+        public string created_at { get; set; }
+
+        public string paid_at { get; set; }
+
+    }
+}
