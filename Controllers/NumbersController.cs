33642a5756aaf0777f10b00fa44a55ae4e3796c5
edgar <edgar.bast.95@gmail.com>
2024-02-28 01:56:16 -0600

    Api numbers

---
+++ Controllers/NumbersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NTTest.Models;
+
+namespace NTTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumbersController : Controller
+    {
+        private readonly Numbers numbersGroup;
+
+        public NumbersController()
+        {
+            numbersGroup = new Numbers();
+        }
+
+        [HttpGet("{numero}")]
+        public ActionResult<int> GetNumeroFaltante(int numero)
+        {
+            try
+            {
+                numbersGroup.Extract(numero);
+                return numbersGroup.CalcularNumeroFaltante();
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Models/Numbers.cs
@@ -0,0 +1,35 @@
+﻿namespace NTTest.Models
+{
+    public class Numbers
+    {
+        private HashSet<int> numeros;
+
+        public Numbers()
+        {
+            numeros = new HashSet<int>(Enumerable.Range(1, 100));
+        }
+
+        public void Extract(int numero)
+        {
+            if (numero < 1 || numero > 100)
+            {
+                throw new ArgumentException("El número debe estar entre 1 y 100.");
+            }
+
+            numeros.Remove(numero);
+        }
+
+        public int CalcularNumeroFaltante()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (!numeros.Contains(i))
+                {
+                    return i;
+                }
+            }
+
+            throw new InvalidOperationException("No se encontró ningún número faltante.");
+        }
+    }
+}
